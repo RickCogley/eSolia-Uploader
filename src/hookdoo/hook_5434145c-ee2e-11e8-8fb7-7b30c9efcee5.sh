2407a6f8f915b42b9f636cdeb67fc4018e79e442
Rick Cogley <rick.cogley@esolia.co.jp>
2018-11-27 08:48:55 +0900

    Variablize jq, call output a json not an out

--- src/hookdoo/hook_5434145c-ee2e-11e8-8fb7-7b30c9efcee5.sh
+++ src/hookdoo/hook_5434145c-ee2e-11e8-8fb7-7b30c9efcee5.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# Runs at Webfaction
+JQBIN="/home/rcogley/bin/jq"
 echo $INPUT > ~/tassy-payload.out
 cat ~/tassy-payload.out | php -R 'echo urldecode($argn)."\n";' > ~/tassy-payload-decoded.out
 cat ~/tassy-payload-decoded.out | awk -F'&signature=' '{print $2}' > ~/tassy-signature.out
 cat ~/tassy-payload-decoded.out | awk -F'&signature=' '{print $1}' > ~/tassy-payload-decoded-pre1.out
 cat ~/tassy-payload-decoded-pre1.out | awk -F'transloadit=' '{print $2}' > ~/tassy-result-pre1.out
-cat ~/tassy-result-pre1.out | /home/rcogley/bin/jq '.' > ~/tassy-result.out
-echo "Done"
+cat ~/tassy-result-pre1.out | ${JQBIN} '.' > ~/tassy-result.json
